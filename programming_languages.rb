2cf1c80bdf3a428a2164b1a88ad16b5a8dc1718d
Emily Wagner <emilywagner598@gmail.com>
2019-01-29 22:41:37 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    languages.each do |type, data|
+      data.each do |k, v|
+        new_hash[k] = {type: v[:type], style: [type]}
+      end
+    end
+    #binding.pry
+    new_hash[:javascript][:style] << :oo
+    new_hash
 end
