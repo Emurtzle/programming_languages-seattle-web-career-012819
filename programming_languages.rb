f057b947e20845769b620f323d77f2e0373b9946
Emily Wagner <emilywagner598@gmail.com>
2019-01-29 22:12:53 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,7 @@ require "pry"
 def reformat_languages(languages)
   new_hash = {}
     languages.each do |type, data|
-      data.each do |lang|
-        new_hash[lang[0]] = {style: type, type: lang[1][:type]}
-      end
+      binding.pry
     end
     new_hash
 end
