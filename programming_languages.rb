6f2a1cbce65d9830ab4b8aa341f27cae3252e653
Emily Wagner <emilywagner598@gmail.com>
2019-01-29 22:17:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
     languages.each do |type, data|
       data.each do |k, v|
         binding.pry
-        new_hash[k] = {v}
+        new_hash[k] = v
       end
     end
     new_hash
